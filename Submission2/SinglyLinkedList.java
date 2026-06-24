528f5f396649f8914619b7e1f58d0da26bcdc951
mahoshadi <mahoshadij@gmail.com>
2026-06-24 22:23:35 +0530

    Create SinglyLinkedList.java

---
+++ Submission2/SinglyLinkedList.java
@@ -0,0 +1,50 @@
+public class SinglyLinkedList {
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node head;
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+
+            temp.next = newNode;
+        }
+    }
+
+    public void display() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.insert(5);
+        list.insert(10);
+        list.insert(15);
+
+        list.display();
+    }
+}
