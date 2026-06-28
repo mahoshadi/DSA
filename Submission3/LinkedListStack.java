9fb792c48be49fd412528028e73daf8d19360580
mahoshadi <mahoshadij@gmail.com>
2026-06-28 23:32:26 +0530

    Add LinkedListStack implementation
    Implement a stack using a linked list with push, pop, and display methods.

---
+++ Submission3/LinkedListStack.java
@@ -0,0 +1,56 @@
+public class LinkedListStack {
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    Node top;
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+
+        newNode.next = top;
+        top = newNode;
+    }
+
+    public int pop() {
+        if (top == null) {
+            return -1;
+        }
+
+        int value = top.data;
+        top = top.next;
+
+        return value;
+    }
+
+    public void display() {
+        Node temp = top;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack stack = new LinkedListStack();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        stack.display();
+
+        System.out.println(stack.pop());
+
+        stack.display();
+    }
+}
