4aca271aaf7a56d8d56088c4bf7e802bd26b1508
mahoshadi <mahoshadij@gmail.com>
2026-06-28 23:33:52 +0530

    Implement RabbitProblem to calculate rabbit pairs

---
+++ Submission3/RabbitProblem.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class RabbitProblem {
+
+    public static int rabbits(int month) {
+        if (month == 1 || month == 2) {
+            return 1;
+        }
+
+        return rabbits(month - 1) + rabbits(month - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of months: ");
+        int month = input.nextInt();
+
+        System.out.println("Number of rabbit pairs: " + rabbits(month));
+    }
+}
